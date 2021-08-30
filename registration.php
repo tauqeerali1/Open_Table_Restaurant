9cb1a4c95cd280195425d9331c96ebc2d9876c0e
Tauqeer Ali <tauqeer.ali.9934@gmail.com>
2021-08-31 01:06:45 +0530

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 header('location:login.html');
-$con=mysqli_connect('lo', '', '', '');
+$con=mysqli_connect('', '', '', '');
 if($con){
 	echo "connection successful";
 }else{
